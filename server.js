8c2360e384dd566da8effacb75566db7208336b0
resyfer <resyfer.dev@gmail.com>
2021-10-24 15:18:11 +0530

    setup file send path

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ require("dotenv").config({ path: "./config/.env" });
     resolvers,
   });
 
-  process.env.NODE_ENV == "production" &&
-    app.get("/", (req, res) => {
-      res.status(200).sendFile(path.resolve("client", "dist", "index.html"));
-    });
-
   await server.start();
   server.applyMiddleware({ app });
   console.log("GraphQL Server Started");
 
   await mongoose.connect(process.env.MONGO_URI);
   console.log("DB Connected");
+
+  process.env.NODE_ENV == "production" &&
+    app.get("*", (req, res) => {
+      res.status(200).sendFile(path.resolve("client", "dist", "index.html"));
+    });
 
   app.listen(process.env.PORT, () => {
     console.log(
